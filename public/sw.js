3126192f801038322e71070743387f19bcac88eb
Julius Plenz <julius@plenz.com>
2025-09-03 13:33:07 +1000

    Move app.html -> index.html and add service worker and manifest

---
+++ public/sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'probly-app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/assets/auto-render.min.js',
+  '/assets/d3.v7.min.js',
+  '/assets/jstat.min.js',
+  '/assets/katex.min.css',
+  '/assets/katex.min.js',
+  '/assets/numeric.min.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
